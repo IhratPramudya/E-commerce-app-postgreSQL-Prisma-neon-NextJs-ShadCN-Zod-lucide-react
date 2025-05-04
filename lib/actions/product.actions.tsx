f35ef631ef6db9e6391ddb7e404552839d74acf2
ihrat pramudya <ihratpramudya>
2025-05-04 16:56:32 +0900

    create add components and api integration for prisma PortgresSQL and neon new tech stack

---
+++ lib/actions/product.actions.tsx
@@ -0,0 +1,21 @@
+'use server';
+import { PrismaClient } from "@prisma/client";
+import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+
+// Get latest products
+export async function getLatestProducts() {
+    const prisma = new PrismaClient();
+
+    const data = await prisma.product.findMany({
+        // Digunakan untuk mengambil 4 produk data terbatas
+        // biasanya ketika ingin mendapatkan data rekomendasi
+        take: LATEST_PRODUCTS_LIMIT, 
+        // Digunakan untuk mengurutkan data berdasarkan data terbaru maupun data terlama
+        // jika menggunakan asc berarti kita mengambil data terlama tetapi jika menggunakan
+        //  desc ini berarti kita mengambil data terbaru yang baru ditambahkan diakhir
+        orderBy: { createdAt: 'desc' }
+    });
+
+    return convertToPlainObject(data);
+}

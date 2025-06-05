439f419c54a6e9ed62fd718ffe94adc0c913e602
ihrat pramudya <ihratpramudya>
2025-06-05 15:45:46 +0900

    add Cart page for detail product items cart add with pay

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -1,5 +1,17 @@
-const CartTable = () => {
-    return (<>Table</>)
+'use client';
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { Cart } from "@/types"; 
+
+interface AddPageProps {
+  cart?: Cart | null;
+}
+
+const CartTable: React.FC<AddPageProps> = ({ cart }) => {
+    return <>
+        <h1 className="py-4 h2-bold">Shopping Cart</h1>
+    </>
 }
 
 export default CartTable;
--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,13 +1,18 @@
 import CartTable from "./cart-table";
+import { getMyCart } from "@/lib/actions/cart.actions";
+import { Cart } from "@/types";
 
 export const metadata = {
     title: 'Shopping Cart',
 }
 
-const CartPage = () => {
+
+const CartPage = async () => {
+    const cart = await getMyCart() as Cart | null; 
+
     return (
         <>
-            <CartTable />
+            <CartTable cart={cart} />
         </>
     )
 }
